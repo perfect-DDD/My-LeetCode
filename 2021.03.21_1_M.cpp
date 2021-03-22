efab14d829949c5b2dd8b0ea84ed2c9a3f58394e
perfect-DDD <3083399835@qq.com>
2021-03-22 23:04:58 +0800

    2021.03.22_1_E

--- 2021.03.21_1_M.cpp
+++ 2021.03.21_1_M.cpp
@@ -1,7 +1,6 @@
 ﻿//给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
 //
 //进阶：
-//
 //一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
 //一个简单的改进方案是使用 O(m  + n) 的额外空间，但这仍然不是最好的解决方案。
 //你能想出一个仅使用常量空间的解决方案吗？
@@ -62,12 +61,12 @@ public:
     }
 };
 
-void main() {
-    vector<vector<int>> matrix = {
-        {1,1,1},
-        {1,0,1},
-        {1,1,1},
-    };
-    mySolution mytest;
-    mytest.setZeroes(matrix);
-}
+//void main() {
+//    vector<vector<int>> matrix = {
+//        {1,1,1},
+//        {1,0,1},
+//        {1,1,1},
+//    };
+//    mySolution mytest;
+//    mytest.setZeroes(matrix);
+//}
---
+++ 2021.03.22_1_E.cpp
@@ -0,0 +1,46 @@
+//编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+//
+//提示：
+//输入必须是长度为 32 的 二进制串 。
+//
+//进阶：
+//如果多次调用这个函数，你将如何优化你的算法？
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/number-of-1-bits
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+#include <iostream>
+using namespace std;
+
+class mySolution {
+public:
+    int hammingWeight(uint32_t n) {
+        int index = 0;
+        for (int i = 0; i < 32; i++) {
+            if (n % 2)
+                index += 1;
+            n = n / 2;
+        }
+        return index;
+    }
+};
+
+class bestSolution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            res++;
+        }
+        return res;
+    }
+};
+
+//void main() {
+//    uint32_t n = 00000000000000000000000000001011;
+//    mySolution mytest;
+//    mytest.hammingWeight(n);
+//}
